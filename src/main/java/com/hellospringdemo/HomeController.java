ea56b0b7e6cb3dafb72ed891c0cef5e645dc053f
prudhika <prudhika@gmail.com>
2020-08-27 16:00:37 -0700

    Spring Demo
    controller &
    1.web.xml
    2.dispatcher-servlet.xml

---
+++ src/main/java/com/hellospringdemo/HomeController.java
@@ -0,0 +1,16 @@
+package com.hellospringdemo;
+
+
+import org.springframework.core.env.SystemEnvironmentPropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+
+    }
+}
